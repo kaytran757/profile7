14cb8f19ee8f208511e008c7f08140a7492d17ce
kaytran757 <kaytran757@gmail.com>
2026-03-02 11:43:24 +0700

    Updated CitySection.tsx

--- src/components/CitySection.tsx
+++ src/components/CitySection.tsx
@@ -62,7 +62,7 @@ const cityModels: Record<string, Model[]> = {
     { name: 'Tuyết Mai', age: 20, district: 'Mỹ Khê', price: 8, height: 162, weight: 50, views: 107.5, tags: ['Bí ẩn'], vip: false, image: 'https://pbs.twimg.com/media/HCYNh6XaMAAZV-3?format=jpg&name=4096x4096' },
     { name: 'Linh Chi', age: 22, district: 'An Hải', price: 11, height: 165, weight: 54, views: 102.8, tags: ['Nhẹ nhàng', 'Dịu dàng'], vip: true, image: 'https://pbs.twimg.com/media/HCYNrFIakAEumEb?format=jpg&name=large' },
     { name: 'Nhã Uyên', age: 26, district: 'Hòa Xuân', price: 13, height: 159, weight: 48, views: 97.4, tags: ['Hài hước'], vip: false, image: 'https://pbs.twimg.com/media/HCYN3hebwAAUIbo?format=jpg&name=4096x4096' },
-    { name: 'Phúc An', age: 19, district: 'Phước Mỹ', price: 9, height: 168, weight: 46, views: 91.6, tags: ['Chiều chuộng', 'Vui vẻ'], vip: false, image: 'https://pbs.twimg.com/media/HCXQeYlaAAAZlRl?format=jpg&name=large' },
+    { name: 'Phúc An', age: 19, district: 'Phước Mỹ', price: 9, height: 168, weight: 46, views: 91.6, tags: ['Chiều chuộng', 'Vui vẻ'], vip: false, image: 'https://pbs.twimg.com/media/HCYOBuxa8AEsOPo?format=jpg&name=4096x4096' },
   ],
   nt: [
     { name: 'Ngọc Hân', age: 23, district: 'Lộc Thọ', price: 14, height: 165, weight: 51, views: 171.5, tags: ['Lãng mạn', 'Chiều chuộng'], vip: true, image: 'https://pbs.twimg.com/media/HCXQeYlaAAAZlRl?format=jpg&name=large' },
